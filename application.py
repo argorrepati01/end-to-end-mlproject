719fd90df5f681ac359b6662ab2a2fc5cdf4850c
argorrepati01 <argorrepati01@gmail.com>
2025-03-11 18:05:47 +0530

    updated app.py

--- application.py
+++ application.py
@@ -34,4 +34,4 @@ def predict_datapoint():
         return render_template('home.html', results=results[0])
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0") #we remove debug=True when we are deploying
+    app.run(host="0.0.0.0", port=8000) #we remove debug=True when we are deploying
